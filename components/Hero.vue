4611b974bf7b7255c3cfe8a6748a1497cfc5a817
daooda <davinhoba9@outlook.fr>
2020-09-19 19:15:51 +0000

    adding Hero description compnents ✅

---
+++ components/Hero.vue
@@ -0,0 +1,81 @@
+<template>
+<section id="motivation" class="section is-medium has-background-white-bis">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column is-5">
+            <h2 class="is-size-2">Galsen Dev</h2>
+            <br />
+            <p class="is-size-5">
+              Galsen Dev est une communauté de développeurs sénégalais et de personnes travaillant autour de la programmation  au Sénégal et nous avons aussi pour objectif de vulgariser la programmation partout au dans le pays et d'aider les débutants et les personnes désireux de rejoindre notre domaine à s'améliorer graduellement.
+            </p>
+            <br />
+            <br />
+            <div class="has-text-centered">
+              <b-button size="is-large" icon-right="github-circle" @click="goToRepo()">
+                Rejoindre
+              </b-button>
+            </div>
+          </div>
+          <div class="column is-offset-1 is-6">
+            <figure class="image is-3by2">
+              <img src="../assets/images/developers-code.svg" class="" alt="Movie Night Illustration" />
+            </figure>
+          </div>
+          <br />
+        </div>
+      </div>
+    </section>
+</template>
+
+<style lang="scss">
+@import '~/assets/scss/gradients.scss';
+.hero {
+  background-size: cover;
+  .hero__logo {
+    min-width: 20rem;
+    max-width: 33%;
+    margin-bottom: 5rem;
+  }
+  .hero__btn {
+    color: white;
+    border: none;
+    @extend .triple-gradient;
+    transition: 0.5s;
+    background-size: 200% auto;
+    &:hover {
+      background-position: right center;
+    }
+  }
+  .scroll-down__arrow {
+    color: white;
+    margin-bottom: 2rem;
+    cursor: pointer;
+  }
+  .bounce {
+    -moz-animation: bounce 2s infinite;
+    -webkit-animation: bounce 2s infinite;
+    animation: bounce 2s infinite;
+  }
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-1rem);
+    }
+    60% {
+      transform: translateY(-0.5rem);
+    }
+  }
+}
+#instructions {
+  .instructions__image {
+    margin: 0 auto;
+    max-width: 60%;
+  }
+}
+</style>
