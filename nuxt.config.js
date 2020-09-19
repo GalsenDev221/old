3b7449bf54fc4a0afa7904c8c5b07c63a4e0457f
daooda <davinhoba9@outlook.fr>
2020-09-19 19:18:08 +0000

    changing HomePage title name ✅

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'galsendev-app',
+    title: 'Galsen Dev | La maison des développeurs sénégalais!',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
