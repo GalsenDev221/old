bc084e2751410d03e58077a01197708cf01528b1
daooda <davinhoba9@outlook.fr>
2020-09-19 02:28:32 +0000

    first commit 💚🚀

---
+++ components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="../assets/galsendev.png"
+                    alt="The home of senegalese developers!"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Acceuil
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Événements
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                A Propos
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Contact
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-info">
+                        <strong>Ressources</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    components: {
+    NavBar
+  }
+}
+</script>
