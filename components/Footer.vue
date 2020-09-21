8e87fb4a75a8e18a50effbe2f4bad584abd727ab
daooda <davinhoba9@outlook.fr>
2020-09-21 22:55:45 +0000

    trying This to resolve branch conflicts 🎯

--- components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <footer class="footer">
-    <div class="content has-text-centered">
-    <p>
-      <strong>💚 Made with 💜</strong> by <strong><a href="https://github.com/Galsen-Dev-LAB" target="_blank">Galsen Dev LAB</a></strong>.
-    </p>
-    </div>
-    </footer>
-</template>
