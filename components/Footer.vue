a38bf85a0593294f8f291695326b4ec62b489bd7
daooda <davinhoba9@outlook.fr>
2021-01-18 01:28:01 +0000

    2021.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
   <div class="content has-text-centered">
     <p>
-      Fait avec 💚 par <strong><a href="https://github.com/Galsen-Dev-LAB" target="_blank">Galsen Dev LAB 💜</a></strong> | Copyright &copy; 2020, Tous Droits Réservés.
+      Fait avec 💚 par <strong><a href="https://github.com/Galsen-Dev-LAB" target="_blank">Galsen Dev LAB 💜</a></strong> | Copyright &copy; 2020-2021, Tous Droits Réservés.
     </p>
   </div>
 </footer>
