3221fa3f71656a52db44d9c1cb25a55fc2655c38
daoodaba975 <davinhoba9@outlook.fr>
2021-08-24 14:28:05 +0000

    robot txt added 🆗

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,19 +8,29 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Galsen Dev | La maison des développeurs sénégalais!',
+    htmlAttrs: { lang: 'fr' },
     meta: [
+      // Meta & description tags
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      // Some meta tag for a better SEO
-      { hid: 'description', name: 'description', content: '' },
-      { property:'og:type', content:'website' },
-      { property:'og:url', content:'https://galsendev.com' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'Galsen Dev | La maison des développeurs sénégalais!' },
+      { name: 'theme-color', content: '#5271FF' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:description', content: 'Galsen DEV' },
+      { property: 'og:url', content: 'https://galsendev.com' },
+      { property:'og:image', content:'https://galsendev.com/static/share.png' },
       { property:'og:description', content:'La maison des développeurs sénégalais!' },
-      { property:'og:image', content:'https://galsendev.com/static/share.png' }
     ],
     link: [
+      // Favicon & App Icons
       { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+      // Google Fonts
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Advent+Pro&display=swap" }
     ],
   },
@@ -46,6 +56,18 @@ export default {
     // https://go.nuxtjs.dev/buefy
     'nuxt-buefy',
     '@nuxtjs/pwa',
+    '@nuxtjs/robots',
+  ],
+
+  robots: [
+    {
+      UserAgent: 'Googlebot',
+      Disallow: '/user',
+    },
+    {
+      UserAgent: '*',
+      Disallow: '/admin',
+    },
   ],
 
   pwa: {
